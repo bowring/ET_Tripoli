1d8d124b14251cf6baf40890fa6aefe6f2d92291
Jim Bowring <bowring@gmail.com>
2022-01-11 16:12:48 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 echo "Hello Travis ****************************************"
 
-wget -o libjava17.tar.gz https://download.bell-sw.com/java/17.0.1+12/bellsoft-jre17.0.1+12-linux-amd64-full.tar.gz
+wget -O  libjava17.tar.gz https://download.bell-sw.com/java/17.0.1+12/bellsoft-jre17.0.1+12-linux-amd64-full.tar.gz
 
 tar -xvf libjava17.tar.gz
